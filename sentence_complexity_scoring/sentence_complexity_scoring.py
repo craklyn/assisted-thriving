7893ab642fb7ce2b0bce7ac6f862bb668614e24a
Daniel Blackburn <daniel.blackburn@globant.com>
2019-07-13 16:10:58 -0700

    rename file, make it more useful as a library

---
+++ sentence_complexity_scoring/sentence_complexity_scoring.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+
+
+df = pd.read_csv("../lexical_simplification/word_complexity_lexicon/lexicon.tsv", sep='\t', names=["word", "complexity"])
+df['word_lower'] = df["word"].str.lower()
+
+
+def score_sentence(sentence):
+    print("Splitting the sentence: " + sentence)
+    print("Split form:")
+    print(sentence.split())
+
+    word_complexity_scores = [df[df['word_lower'] == word.lower()]["complexity"].iloc[0] if any(word.lower() == df["word_lower"]) else 5.0 for word in sentence.split()]
+    return {"word_complexity_scores": word_complexity_scores,
+            "total_score": np.nansum(word_complexity_scores) / len(word_complexity_scores)}
+
+sentences = ["I am a hungry caterpillar",
+    "Beyond my might for tonight I shall dine on turtle soup",
+    "Tonight I will eat soup",
+    "I eat soup"]
+
+
+if __name__ == "__main__":
+    for sentence in sentences:
+        print(sentence)
+        sentence_score = score_sentence(sentence)
+        print(sentence_score['word_complexity_scores'])
+        print(sentence_score['total_score'])
+
+
