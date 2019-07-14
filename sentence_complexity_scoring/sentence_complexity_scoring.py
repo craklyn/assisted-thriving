f51419dc8ad9c0af1933523830e794c4387c1610
Daniel Blackburn <daniel.blackburn@globant.com>
2019-07-14 10:08:51 -0700

    latest snapshot

--- sentence_complexity_scoring/sentence_complexity_scoring.py
+++ sentence_complexity_scoring/sentence_complexity_scoring.py
@@ -6,14 +6,35 @@ df = pd.read_csv("../lexical_simplification/word_complexity_lexicon/lexicon.tsv"
 df['word_lower'] = df["word"].str.lower()
 
 
-def score_sentence(sentence):
-    print("Splitting the sentence: " + sentence)
-    print("Split form:")
-    print(sentence.split())
+def vocab_level(sentence):
 
     word_complexity_scores = [df[df['word_lower'] == word.lower()]["complexity"].iloc[0] if any(word.lower() == df["word_lower"]) else 5.0 for word in sentence.split()]
     return {"word_complexity_scores": word_complexity_scores,
             "total_score": np.nansum(word_complexity_scores) / len(word_complexity_scores)}
+
+
+# This function is useless.  Don't use it.
+def potential_vocabulary_size(sentences):
+    words = [word.lower() for sentence in sentences for word in sentence.split()]
+    known_words = set([word for word in words if any(word == df["word_lower"])])
+
+    N = len(words)
+    print(df.size)
+    n = df.shape[0]
+
+    numer = 0
+    for i in range(len(known_words)):
+        numer += 1./(i+1)
+
+    denom = 0
+    for i in range(df.shape[0]):
+        denom += (1. / (i+1))
+
+    print("Numer: " + str(numer))
+    print("Denom: " + str(denom))
+
+    return numer / denom
+
 
 sentences = ["I am a hungry caterpillar",
     "Beyond my might for tonight I shall dine on turtle soup",
@@ -24,8 +45,16 @@ sentences = ["I am a hungry caterpillar",
 if __name__ == "__main__":
     for sentence in sentences:
         print(sentence)
-        sentence_score = score_sentence(sentence)
+
+        print("Sentence score:")
+        sentence_score = vocab_level(sentence)
         print(sentence_score['word_complexity_scores'])
         print(sentence_score['total_score'])
+
+        print("Potential vocabulary ndim:")
+        pvs = potential_vocabulary_size([sentence])
+        print(pvs)
+  
+        print("")
 
 
